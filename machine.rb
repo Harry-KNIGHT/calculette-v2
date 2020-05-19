86778dfcf949f4c12e547e8061a94a5a5a896217
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-19 21:35:23 +0200

    Line delimitation

--- machine.rb
+++ machine.rb
@@ -1,3 +1,12 @@
+def line
+
+    20.times {print ('-')}
+    puts " "
+    
+end
+
+
+
 numbers = ARGV[0].to_i
 
 if numbers.even? 
@@ -7,16 +16,19 @@ else
 end
 print "\n"
 
+line
 
-array = 0..90
+array = 0..100
 array = array.to_a
 i = 0
+
 while i < array.length
     if array[i].odd?
         print "#{array[i]}, " 
     end
 i += 1
 end
+print "\n"
 
 
 
